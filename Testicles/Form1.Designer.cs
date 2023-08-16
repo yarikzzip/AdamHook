47680a21e8779a75b454c5c0288d0a6ba81439db
adamj <adamj@POOP>
2023-08-16 22:33:33 +1000

    Add project files.

---
+++ Testicles/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Testicles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(127, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tagswitch";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(15, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(209, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Error";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(7, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Country ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(7, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(503, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter country ID and press tagswitch to change countries";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(221, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Error";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(12, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 42);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "On";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DimGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(118, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 42);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Off";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(274, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Error";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(118, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DimGray;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(12, 309);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 42);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "On";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DimGray;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(118, 407);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 42);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Off";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DimGray;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(12, 407);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 42);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "On";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(242, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Error";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(12, 377);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Debug (1 = on, 0 = off):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(7, 279);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(252, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "AllowTraits (1 = on, 0 = off):";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(7, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(199, 25);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "FOW (1= on, 0 = off):";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(423, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(337, 100);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "This menu was made by AdamDX\r\nIt automatically attaches to your game\r\nso have it " +
+    "open!\r\nVisit the link below for my yt!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.linkLabel1.Location = new System.Drawing.Point(475, 179);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(168, 46);
+            this.linkLabel1.TabIndex = 19;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Youtube";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 567);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AdamHook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Testicles/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory.Win64;
+using Memory.Utils;
+
+namespace Testicles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ulong targetAddrTS;
+        ulong baseAddrFOW, baseAddrDBG, baseAddrAT;
+        MemoryHelper64 helper;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process p = Process.GetProcessesByName("hoi4").FirstOrDefault();
+            if (p == null) return;
+
+            helper = new MemoryHelper64(p);
+
+            // getting addresses for FOW(0x24C945A), tagswitch TS(0x25E9D00) (0xE40), debug DBG(0x25E9829) and allowtraits AT(0x24C9438)
+
+            //Tag Switch
+            ulong baseAddrTS = helper.GetBaseAddress(0x25E9D00);
+            int[] offsetTS = { 0xE40 };
+            targetAddrTS = MemoryUtils.OffsetCalculator(helper, baseAddrTS, offsetTS);
+
+
+            //These ones only need base address because I got the static address without pointers, only tag switching needs pointers
+            //Also Only Tagswitch is 4bytes, everything else is a byte
+
+            //FOW
+            baseAddrFOW = helper.GetBaseAddress(0x24C945A);
+
+            //AllowTraits
+            baseAddrAT = helper.GetBaseAddress(0x24C9438);
+
+            //Debug
+            baseAddrDBG = helper.GetBaseAddress(0x25E9829);
+
+
+            //DYNAMIC LABELS
+            label1.Text = helper.ReadMemory<Int32>(targetAddrTS).ToString();
+            label4.Text = helper.ReadMemory<Byte>(baseAddrFOW).ToString();
+            label5.Text = helper.ReadMemory<Byte>(baseAddrAT).ToString();
+            label6.Text = helper.ReadMemory<Byte>(baseAddrDBG).ToString();
+            
+            //MANUAL LABELS
+            label2.Text = "Current Country ID: ";
+            label3.Text = "Enter country ID and press tagswitch to change countries";
+            timer1.Start();
+        }
+
+      
+
+
+        //Refresh to see new memory value
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            label1.Text = helper.ReadMemory<Int32>(targetAddrTS).ToString();
+            label4.Text = helper.ReadMemory<Byte>(baseAddrFOW).ToString();
+            label5.Text = helper.ReadMemory<Byte>(baseAddrAT).ToString();
+            label6.Text = helper.ReadMemory<Byte>(baseAddrDBG).ToString();
+
+
+        }
+
+        //BUTTONS
+        private void button1_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Int32>(targetAddrTS, Int32.Parse(textBox1.Text));
+        }  
+        private void button7_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrDBG, Byte.Parse("1"));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrFOW, Byte.Parse("0"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrAT, Byte.Parse("0"));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrDBG, Byte.Parse("0"));
+        }
+
+       
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrAT, Byte.Parse("1"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            helper.WriteMemory<Byte>(baseAddrFOW, Byte.Parse("1"));
+        } 
+
+        //Youtube Link
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.youtube.com/@AdamDX1337");
+        }
+    }
+}
